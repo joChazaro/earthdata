244278e9ed7a82f6bdf115b972582b30f48ce7b3
joChazaro <johana.chazaro@gmail.com>
2023-11-29 18:55:13 -0500

    fixed json url construction

--- earthdata_to_s3.sh
+++ earthdata_to_s3.sh
@@ -7,23 +7,23 @@ function usage {
   echo ""
   echo "Description:"
   echo "  This script will recursively download files for a specified range of Julian days"
-  echo "  from a LAADS URL and upload them to an S3 bucket with folders for each day"
+  echo "  from a LAADS URL and create folders for each day in the destination path"
   echo ""
   echo "Options:"
-  echo "    -u|--url [URL]            Base URL for LAADS data (e.g., https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5019/XAERDT_L2_ABI_G16)"
-  echo "    -b|--bucket [bucket]      S3 bucket name"
+  echo "    -u|--url [URL]            Base URL for LAADS data (e.g., https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/5019/XAERDT_L2_ABI_G16/2019/)"
+  echo "    -d|--destination [path]   Destination directory to store files"
   echo "    -t|--token [token]        Use app token [token] to authenticate"
   echo "    -s|--start [day]          Start Julian day"
   echo "    -e|--end [day]            End Julian day"
   ""
   echo "Dependencies:"
-  echo "  Requires 'jq' and 'awscli' which should be installed"
+  echo "  Requires 'jq' which should be installed"
 }
 
-# Function to recursively download files for a range of Julian days and upload to S3
-function download_and_upload_to_s3 {
+# Function to recursively download files for a range of Julian days and create folders
+function download_and_create_folders {
   local base_url=$1
-  local s3_bucket=$2
+  local destination=$2
   local token=$3
   local start_julian_day=$4
   local end_julian_day=$5
@@ -31,27 +31,24 @@ function download_and_upload_to_s3 {
   for ((julian_day=start_julian_day; julian_day<=end_julian_day; julian_day++))
   do
     julian_day_str=$(printf "%03d" $julian_day)
-    url="${base_url}/$(date -d "2019-01-01 +$((julian_day-1)) days" +"%Y/%j")"
-    
-    echo "Querying ${url}.json for Julian day ${julian_day_str}"
+    url="${base_url}${julian_day}.json"
 
-    for file in $(curl -L -s -g -H "Authorization: Bearer ${token}" -C - ${url}.json | jq '.content | .[] | select(.size!=0) | .name' | tr -d '"')
+    # Create a folder for each day
+    day_folder="${destination}/${julian_day_str}"
+    mkdir -p "${day_folder}"
+
+    # Download files to the day folder
+    echo "Querying ${url} for Julian day ${julian_day_str}"
+    for file in $(curl -L -s -g -H "Authorization: Bearer ${token}" -C - ${url} | jq '.content | .[] | select(.size!=0) | .name' | tr -d '"')
     do
-      local local_file="${file}"
+      local local_file="${day_folder}/${file}"
 
-      # Check if the local file exists before attempting to upload
-      if [ -f ${local_file} ] 
+      if [ ! -f ${local_file} ] 
       then
         echo "Downloading $file to ${local_file}"
-        curl -L -s -g -H "Authorization: Bearer ${token}" -C - ${url}/${file} -o ${local_file}
-
-        # Use only the three digits of the Julian day for the S3 folder name
-        s3_folder_name="${julian_day_str}"
-
-        echo "Uploading $local_file to s3://${s3_bucket}/${s3_folder_name}/${file}"
-        aws s3 cp ${local_file} "s3://${s3_bucket}/${s3_folder_name}/${file}"
+        curl -L -s -g -H "Authorization: Bearer ${token}" -C - "${base_url}${julian_day}/${file}" -o ${local_file}
       else
-        echo "Skipping $file because the local file does not exist."
+        echo "Skipping $file ..."
       fi
     done
   done
@@ -69,8 +66,8 @@ do
     shift # past argument
     shift # past value
     ;;
-    -b|--bucket)
-    s3_bucket="$2"
+    -d|--destination)
+    destination="$2"
     shift # past argument
     shift # past value
     ;;
@@ -104,9 +101,9 @@ then
   exit 1
 fi
 
-if [ -z ${s3_bucket+x} ]
+if [ -z ${destination+x} ]
 then 
-  echo "S3 bucket name is not specified"
+  echo "Destination path is not specified"
   usage
   exit 1
 fi
@@ -132,6 +129,6 @@ then
   exit 1
 fi
 
-# Invoking the download and upload to S3 function for the specified Julian day range
-download_and_upload_to_s3 "$base_url" "$s3_bucket" "$token" "$start_julian_day" "$end_julian_day"
+# Invoking the download and create folders function for the specified Julian day range
+download_and_create_folders "$base_url" "$destination" "$token" "$start_julian_day" "$end_julian_day"
 
